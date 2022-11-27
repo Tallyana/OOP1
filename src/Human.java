2095bfe39663b93b3b2b3fd5720b34844d761bd5
Admin <iggla67@yandex.ru>
2022-11-27 04:24:10 +0400

    1 и 2 домашнее задание ООП.

---
+++ src/Human.java
@@ -0,0 +1,12 @@
+public class Human {
+    String name;
+    int birthday ;
+    String cityOfResidence;
+    String post;
+    //--------------------------------
+    void toSay(){
+        System.out.println("Привет.Меня зовут " + name + ".Мне " + birthday + " лет.Я живу в городе "+ cityOfResidence+ ".Я работаю на должности " + post+ ". Будем знакомы!");
+    }
+}
+
+
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,33 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Human human1 = new Human();
+        Human human2 = new Human();
+        Human human3 = new Human();
+        Human human4 = new Human();
+
+        human1.name = "Максим";
+        human2.name = "Аня";
+        human3.name = "Катя";
+        human4.name = "Артем";
+
+        human1.birthday = 35;
+        human2.birthday = 29;
+        human3.birthday = 28;
+        human4.birthday = 27;
+
+        human1.cityOfResidence = "Минск";
+        human2.cityOfResidence = "Москва";
+        human3.cityOfResidence = "Калининград";
+        human4.cityOfResidence = "Петербург";
+
+        human1.post = "бренд-менеджерa";
+        human2.post = "методистa образовательных программ";
+        human3.post = "продакт-менеджера";
+        human4.post = "директора по развитию бизнеса";
+
+        human1.toSay();
+        human2.toSay();
+        human3.toSay();
+        human4.toSay();
     }
 }
